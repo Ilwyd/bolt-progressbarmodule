e6c734a9a4b33bd250f75331259fca0cd901f679
Ilwyd <wgray5093@gmail.com>
2025-09-27 21:51:08 -0300

    Include offset data in texturedata file

--- interfaces/bars.lua
+++ interfaces/bars.lua
@@ -4,15 +4,17 @@ local texturedata = require("texturedata")
 -- Gets the current percentage of the secondary bar
 -- Used for smithing item heat, archaeology, etc.
 -- Might not provide an accurate value for adrenaline
+local bartexturedata = texturedata.interface.bars
 local secondarybarpercentage = nil
 local function getsecondarybarpercentage()
-    local MAX_DIFF = 56
+    local MAX_DIFF = 56 -- Bar is always the same width
     bolt.onrender2d(function (event)
         local xposes = {}
         for i = 1, event:vertexcount() do
             local ax, ay, aw, ah = event:vertexatlasdetails(i)
+            local secondarybartexturedata = bartexturedata.secondarybar
 
-            if not event:texturecompare(ax, ay + 3, texturedata.interface.bars.secondarybar) then
+            if not event:texturecompare(ax, ay + secondarybartexturedata.offset, secondarybartexturedata.data) then
                 goto continue
             end
 
